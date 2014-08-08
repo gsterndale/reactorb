a52bd8ec50c68dc721aa232f4b97a74198fba005
Greg Sterndale <gsterndale@gmail.com>
2014-08-08 14:41:51 +0200

    Simple TCP server for benchmarks responds with HTML

--- benchmarks/ohai.rb
+++ benchmarks/ohai.rb
@@ -3,15 +3,30 @@
 
 require 'socket'
 
-server = TCPServer.new "localhost", 8888
+verbose = false
+host = "localhost"
+port = 8888
+puts "Starting simple, threaded TCP Server on http://#{host}:#{port}"
+
+server = TCPServer.new host, port
 
 begin
   loop do
     Thread.start(server.accept) do |client|
-      client.puts "Hello!"
-      client.puts "Time is #{Time.now}"
+      request_lines = []
+      while line = client.gets and line !~ /^\s*$/
+        request_lines << line.chomp
+      end
+      puts request_lines.first if verbose
+
+      resp = "<html><body><h1>Time is #{Time.now}</h1></body></html>"
+      headers = ["http/1.1 200 ok",
+                 "content-type: text/html",
+                 "content-length: #{resp.length}\r\n\r\n"].join("\r\n")
+      client.puts headers
+      client.puts resp
       client.close
-      print '.'
+      verbose ? puts("200 OK") : print('.')
     end
   end
 rescue Interrupt
