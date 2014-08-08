ef4beba3b630b6d4c3c1e8cfb542ecf15c20a3c4
Greg Sterndale <gsterndale@gmail.com>
2014-08-08 15:20:36 +0200

    RSpec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,3 +6,12 @@ require File.expand_path("../../lib/reactorb", __FILE__)
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
+RSpec.configure do |config|
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+end
