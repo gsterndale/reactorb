13d3e440a06cf406eba08c08c186321c305bd8c1
Greg Sterndale <gsterndale@gmail.com>
2014-08-11 23:18:50 +0200

    Refactor Future. Better var name & no const lookup

--- lib/reactorb/future.rb
+++ lib/reactorb/future.rb
@@ -5,13 +5,15 @@ class Reactor
   class Future < Delegator
     def self.start(&block)
       Future.new.tap do |future|
-        Fiber.new do
-          f = Fiber.current
-          handler = -> (delegate_obj) { f.resume(delegate_obj) }
+        ::Fiber.new do
+          fiber = ::Fiber.current
+          handler = -> (delegate_obj) do
+            fiber.resume(delegate_obj)
+          end
           block.call(handler)
-          # Yield control back to context that resume()d current fiber (i.e. Reactor#run()'s Fiber).
-          # Then Fiber.yield will return the delegate object once this fiber is resume()d by handler
-          future.__setobj__ Fiber.yield
+          # Yield control back to context that resume()d current fiber (i.e. Reactor#run()'s ::Fiber).
+          # Then ::Fiber.yield will return the delegate object once this fiber is resume()d by handler
+          future.__setobj__ ::Fiber.yield
         end.resume
       end
     end
@@ -24,8 +26,8 @@ class Reactor
 
     def __getobj__
       if !@obj_set
-        @fiber = Fiber.current
-        Fiber.yield
+        @fiber = ::Fiber.current
+        ::Fiber.yield
       end
       @delegate_sd_obj
     end
